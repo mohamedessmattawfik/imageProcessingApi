0e1a15f902031b8d51b99ff3ac58f994f65a7e98
MohamedAhmed <mohamed.essmattawfik@gmail.com>
2022-09-09 17:32:57 +0200

    refactor: add return types to callback functions

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import routes from './routes/index';
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`Listening on port ${port}🚀🚀🚀`));
-app.get('/', (req: Request, res: Response) => {
+app.listen(port, (): void => console.log(`Listening on port ${port}🚀🚀🚀`));
+app.get('/', (req: Request, res: Response): void => {
   res.send(
     'Welcome to the first project in Advanced Web Development Nanodegree'
   );
